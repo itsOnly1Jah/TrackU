7fd9517d35d152ff529f16af457dbe86b78a9a96
itsOnly1Jah <kingjah23@gmail.com>
2024-02-08 13:01:01 -0500

    refactor: remove nodejs and move everything to Flask

--- src/tracku.py
+++ src/tracku.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template
+from flask_assets import Bundle, Environment
 
 
 app = Flask(__name__)
+
+
+assets = Environment(app)
+css = Bundle("css/styles.css", output="css/tailwind.css")
+
+assets.register("css", css)
+css.build()
 
 trackers = {
     "Amazon": [34, 55, 70, 91, 65, 55, 40],
@@ -14,6 +22,11 @@ db_entries = [
     {"name": "XBOX", "tracker": trackers},
     {"name": "PS5", "tracker": trackers}
 ]
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
 
 
 @app.route("/table")
